70a268c575443ac6e7437f6de1bd149882c04d2f
mayankk-XD <singh.mayank7720@gmail.com>
2026-09-10 20:29:09 +0530

    Added calculation funcionality for 2 operands

--- script.js
+++ script.js
@@ -1,31 +1,82 @@
 const buttonArea = document.querySelector(".buttonsArea");
+const displayArea = document.querySelector(".displayArea");
 
 let buttonList = [["7" , "8" , "9" , "/"], ["4", "5", "6", "*"], ["1", "2", "3", "-"], ["0", ".", "=", "+"]];
+let operatorsList = ["+" , "-", "*", "/"];
 
+//creating the rows in which buttons will be placed
 for(let i = 0 ; i < 4 ; i++){
     const rows = document.createElement("div")
     rows.classList.add("lowerButtons")
     buttonArea.appendChild(rows);
+
+    //creating and adding the buttons in the created rows 
     for(let j = 0 ; j < 4 ; j++){
         const newButton = document.createElement("button");
         newButton.classList.add("heyButton");
         newButton.textContent = buttonList[i][j];
         rows.appendChild(newButton);
+
+        //event listener for each button created
         newButton.addEventListener("click", () => {
-            displayy(newButton.textContent);
+            displayyOperations(newButton.textContent);
         });
     }
 }
 
-const displayArea = document.querySelector(".displayArea");
+//showing default 0 value in the display area 
 displayArea.textContent = "0";
 
+let firstOperand = "";
+let secondOperand = "";
+let operator = ""
 
-function displayy(content){
-    if (displayArea.textContent === "0") {
-        displayArea.textContent = content;
+
+function displayyOperations(content){
+
+    if(content === "="){
+        secondOperand = displayArea.textContent;
+        firstOperand = calculations(firstOperand, secondOperand, operator);
+        displayArea.textContent = secondOperand;
+    } 
+
+    else if(operatorsList.includes(content)){
+        if(firstOperand === "" && operator === ""){
+            firstOperand = displayArea.textContent;
+            operator = content;
+            displayArea.textContent = "";
+        }
+        else{
+            secondOperand = displayArea.textContent;
+            firstOperand = calculations(firstOperand, secondOperand, operator);
+            operator = content;
+            displayArea.textContent = "";
+        }
+
     } 
     else {
-        displayArea.textContent += content;
+        if (displayArea.textContent === "0") displayArea.textContent = content;
+        else displayArea.textContent += content;
     }
+}
+
+
+function calculations(operendFirst, operandSecond, operator) {
+    let res = 0;
+
+    if (operator === "+") {
+        res = Number(operendFirst) + Number(operandSecond);
+    }
+    else if (operator === "-") {
+        res = Number(operendFirst) - Number(operandSecond);
+    }
+    else if (operator === "*") {
+        res = Number(operendFirst) * Number(operandSecond);
+    }
+    else if (operator === "/") {
+        if(Number(operandSecond) === 0) return "Can't Divide by 0"
+        res = Number(operendFirst) / Number(operandSecond);
+    }
+
+    return String(res);
 }
